859c1630251228eddcd1e8557e98981a62302517
Akash Bora <89206401+Akascape@users.noreply.github.com>
2023-05-14 12:43:34 +0530

    Fixed button text color

--- CTkScrollableDropdown/__init__.py
+++ CTkScrollableDropdown/__init__.py
@@ -6,6 +6,6 @@ This is a custom dropdown menu for customtkinter.
 Homepage: https://github.com/Akascape/CTkScrollableDropdown
 """
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 from .ctk_scrollable_dropdown import CTkScrollableDropdown
