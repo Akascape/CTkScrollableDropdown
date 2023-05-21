94355ebfae893670870730ed7d5c7e688dc75566
Akash Bora <89206401+Akascape@users.noreply.github.com>
2023-05-21 22:40:45 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from CTkScrollableDropdown import CTkScrollableDropdown
+from CTkScrollableDropdown import *
 import customtkinter
 
 root = customtkinter.CTk()
@@ -6,35 +6,29 @@ root = customtkinter.CTk()
 customtkinter.CTkLabel(root, text="Different Dropdown Styles").pack(pady=5)
 
 # Some option list
-values = ["python","tkinter","customtkinter","widgets","options","menu","combobox","dropdown","search"]
+values = ["python","tkinter","customtkinter","widgets",
+          "options","menu","combobox","dropdown","search"]
 
 # Attach to OptionMenu 
 optionmenu = customtkinter.CTkOptionMenu(root, width=240)
 optionmenu.pack(fill="x", padx=10, pady=10)
 
-CTkScrollableDropdown(optionmenu, values=values, command=lambda e: optionmenu.set(e))
+CTkScrollableDropdown(optionmenu, values=values)
 
 # Attach to Combobox
 combobox = customtkinter.CTkComboBox(root, width=240)
 combobox.pack(fill="x", padx=10, pady=10)
-CTkScrollableDropdown(combobox, values=values, justify="left", button_color="transparent", command=lambda e: combobox.set(e))
 
-# Live Entry Search
-def search(a,b,c):
-    dropdown_3.live_update(var.get())
-    
-var = customtkinter.StringVar()
-var.trace_add('write', search)
+CTkScrollableDropdown(combobox, values=values, justify="left", button_color="transparent")
 
+# Attach to Entry
 customtkinter.CTkLabel(root, text="Live Search Values").pack()
 
-entry = customtkinter.CTkEntry(root, width=240, textvariable=var)
+entry = customtkinter.CTkEntry(root, width=240)
 entry.pack(fill="x", padx=10, pady=10)
 
-dropdown_3 = CTkScrollableDropdown(entry, values=values, command=lambda e: var.set(e))
-
-# the same trace method can be implemented to combobox using:
-# combobox._entry.configure(textvariable=var)
+CTkScrollableDropdown(entry, values=values, command=lambda e: entry.insert(1, e),
+                      autocomplete=True) # Using autocomplete
 
 # Attach to Button 
 button = customtkinter.CTkButton(root, text="choose options", width=240)
