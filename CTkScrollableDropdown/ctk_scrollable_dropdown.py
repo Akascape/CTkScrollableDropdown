08adc7badbf2ac5d9770b81c7ca17973ee716d80
Akash Bora <89206401+Akascape@users.noreply.github.com>
2023-05-21 17:41:32 +0530

    Update ctk_scrollable_dropdown.py

--- CTkScrollableDropdown/ctk_scrollable_dropdown.py
+++ CTkScrollableDropdown/ctk_scrollable_dropdown.py
@@ -243,9 +243,6 @@ class CTkScrollableDropdown(customtkinter.CTkToplevel):
     def _deiconify(self):
         if len(self.values)>0:
             self.deiconify()
-            
-    def popup(self):
-        self._iconify()
         
     def configure(self, **kwargs):
         if "height" in kwargs:
